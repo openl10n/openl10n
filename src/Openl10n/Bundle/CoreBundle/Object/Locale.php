10409ad128f8ee376e15756f252b3fb883705c96
Benjamin Laugueux <benjamin@laugueux.org>
2013-12-28 19:19:10 +0100

    Added flags in UI.
    Improved flags management.
    
    Fixed failing behat tests.
    
    Removed flag when locale does not contain region.
    
    Added flag mapping for locales without region.

--- src/Openl10n/Bundle/CoreBundle/Object/Locale.php
+++ src/Openl10n/Bundle/CoreBundle/Object/Locale.php
@@ -12,6 +12,53 @@ class Locale
      */
     protected $locale;
 
+    protected $flagMapping = array(
+        'ar' => 'ae',
+        'be' => 'by',
+        'bg' => 'bg',
+        'ca' => 'es',
+        'cs' => 'cz',
+        'da' => 'dk',
+        'de' => 'de',
+        'el' => 'gr',
+        'en' => 'gb',
+        'es' => 'es',
+        'et' => 'ee',
+        'fi' => 'fi',
+        'fr' => 'fr',
+        'ga' => 'ie',
+        'hi' => 'in',
+        'hr' => 'hr',
+        'hu' => 'hu',
+        'in' => 'id',
+        'is' => 'is',
+        'it' => 'it',
+        'iw' => 'il',
+        'ja' => 'jp',
+        'ko' => 'kr',
+        'lt' => 'lt',
+        'lv' => 'lv',
+        'mk' => 'mk',
+        'ms' => 'my',
+        'mt' => 'mt',
+        'nl' => 'nl',
+        'no' => 'no',
+        'pl' => 'pl',
+        'pt' => 'pt',
+        'ro' => 'ro',
+        'ru' => 'ru',
+        'sk' => 'sk',
+        'sl' => 'si',
+        'sq' => 'al',
+        'sr' => 'rs',
+        'sv' => 'se',
+        'th' => 'th',
+        'tr' => 'tr',
+        'uk' => 'ua',
+        'vi' => 'vn',
+        'zh' => 'cn',
+    );
+
     /**
      * Build a locale object with a valid string.
      *
@@ -20,6 +67,22 @@ class Locale
     public function __construct($locale)
     {
         $this->locale = \Locale::canonicalize($locale);
+    }
+
+    /**
+     * Gets the flag for the locale or 'unknown'
+     *
+     * @return string The flag code or 'unknown'
+     */
+    public function getFlag()
+    {
+        if ($this->getRegion() !== '') {
+            return strtolower($this->getRegion());
+        } else if (array_key_exists($this->getPrimaryLanguage(), $this->flagMapping)) {
+            return $this->flagMapping[$this->getPrimaryLanguage()];
+        } else {
+            return 'unknown';
+        }
     }
 
     /**
@@ -52,6 +115,32 @@ class Locale
         return $inLocale ?
             \Locale::getDisplayName($this->locale, $inLocale) :
             \Locale::getDisplayName($this->locale)
+        ;
+    }
+
+    /**
+     * Returns an appropriately localized display language for the locale.
+     *
+     * @return string The display language
+     */
+    public function getDisplayLanguage($inLocale = null)
+    {
+        return $inLocale ?
+            \Locale::getDisplayLanguage($this->locale, $inLocale) :
+            \Locale::getDisplayLanguage($this->locale)
+        ;
+    }
+
+    /**
+     * Returns an appropriately localized display region for the locale.
+     *
+     * @return string The display region
+     */
+    public function getDisplayRegion($inLocale = null)
+    {
+        return $inLocale ?
+            \Locale::getDisplayRegion($this->locale, $inLocale) :
+            \Locale::getDisplayRegion($this->locale)
         ;
     }
 
