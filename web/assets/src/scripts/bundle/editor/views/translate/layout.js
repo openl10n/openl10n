106a9bc510e7265265c292882a12c717b1142a0b
Matthieu Moquet <matthieu@moquet.net>
2014-05-27 23:21:02 +0200

    Auto focus on editor textarea

--- web/assets/src/scripts/bundle/editor/views/translate/layout.js
+++ web/assets/src/scripts/bundle/editor/views/translate/layout.js
@@ -6,10 +6,19 @@ define([
   var TranslateLayoutView = Marionette.Layout.extend({
     template: translateLayoutTpl,
 
+    ui: {
+      textarea: 'textarea.phrase-editor'
+    },
+
     regions: {
       tabsRegion: '#edit-tabs',
       informationTabRegion: '#tab-information',
       historyTabRegion: '#tab-history',
+    },
+
+    onShow: function() {
+      // this.ui.textarea.get(0).focus();
+      this.ui.textarea.select();
     }
   });
 
