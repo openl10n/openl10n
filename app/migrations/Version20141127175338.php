09ef97edeb0b3b249177efc906b7be220c49c454
Matthieu Moquet <matthieu@moquet.net>
2014-11-27 17:55:10 +0100

    Add migration script due to column name change
    Change has been made in 1f32dddbd7fa425f426fb6e865dfd20c797c8213

---
+++ app/migrations/Version20141127175338.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Openl10n\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141127175338 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE users CHANGE prefered_locale preferred_locale VARCHAR(64) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE users CHANGE preferred_locale prefered_locale VARCHAR(64) NOT NULL');
+    }
+}
